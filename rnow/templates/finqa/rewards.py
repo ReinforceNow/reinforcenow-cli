05542ebaa93c88c68175671788f599284fc873c6
Guido Trevisan <guidotrevisan6@gmail.com>
2026-01-14 18:50:35 -0600

    updated finqa

--- rnow/templates/finqa/rewards.py
+++ rnow/templates/finqa/rewards.py
@@ -1,14 +1,7 @@
-from math_verify import LatexExtractionConfig, parse, verify
-
 from rnow.core import RewardArgs, get_response, reward
 
 
 @reward
 def answer_correctness(args: RewardArgs, messages: list) -> float:
-    gold = parse(args.metadata["answer"])
-    pred = parse(
-        get_response(messages), extraction_config=[LatexExtractionConfig(boxed_match_priority=0)]
-    )
-    if not pred:
-        return 0.0
-    return 1.0 if verify(gold, pred) else 0.0
+    answer = str(args.metadata["answer"])
+    return 1.0 if answer in get_response(messages) else 0.0
